298fd70b23c349dfa15d0d691e630cb6a75993b7
Dominic Pau <Dominic>
2021-09-03 16:34:33 +0100

    feat: implement functional-tests module

---
+++ functional-tests/src/test/java/uk/codingplayroom/bls/ft/config/BlsContextConfig.java
@@ -0,0 +1,9 @@
+package uk.codingplayroom.bls.ft.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("uk.codingplayroom.bls.ft.*")
+public class BlsContextConfig {
+}
