237b924a67613b7adfc84748115430d614910df0
Dominic Pau <dominic.pau@sky.uk>
2021-10-28 18:08:25 +0100

    feat: add Book and BookService

--- book-library-service/src/main/java/uk/codingplayroom/bls/service/controller/Book.java
+++ book-library-service/src/main/java/uk/codingplayroom/bls/service/controller/Book.java
@@ -1,2 +1,14 @@
-package uk.codingplayroom.bls.service.Model;public class Book {
+package uk.codingplayroom.bls.service.controller;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class Book {
+    private String title;
+    private String author;
+    private int pages;
 }
--- book-library-service/src/main/java/uk/codingplayroom/bls/service/controller/BookService.java
+++ book-library-service/src/main/java/uk/codingplayroom/bls/service/controller/BookService.java
@@ -1,2 +1,36 @@
-package uk.codingplayroom.bls.service.controller;public class BookService {
+package uk.codingplayroom.bls.service.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BookService {
+
+    private List<Book> bookList = new ArrayList<>();
+    private Book emptyBook;
+
+    public List<Book> returnAllBooks() {
+        return bookList;
+    }
+
+    public Book returnBookByTitle(String requestedTitle) {
+        Optional<Book> book = bookList.stream()
+                .filter((x) -> x.getTitle().equals(requestedTitle))
+                .findFirst();
+
+        return book.orElseGet(() -> emptyBook);
+    }
+
+    @Bean
+    public void generateBookList() {
+        Book book1 = new Book("harrypotter", "jkrowling", 2500);
+        Book book2 = new Book("percyjackson", "rickriodian", 2500);
+
+        bookList.add(book1);
+        bookList.add(book2);
+    }
 }
